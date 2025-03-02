83b410771ef609f5618010b685e505c26881d70b
rmungar <mgraul@iesrafaelalberti.es>
2025-03-02 18:48:35 +0100

    Navegacion a la primera pantalla

--- codigo.py
+++ codigo.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 
-#importamos las librerías necesarias
 import sys
-from PyQt6.QtWidgets import * 
-from PyQt6 import uic  
-from PyQt6.QtCore import Qt
-from pathlib import Path
-import re
-import math as Math
-from PyQt6.QtGui import QIcon
 import os
-import pyrebase as pyrebase
+import pyrebase
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QErrorMessage
+from PyQt6 import uic
+
+basedir = os.path.dirname(__file__)
 
 config = {
     "apiKey": "AIzaSyB7fn2d5cm8YtiBrelexmoxleAezCNiU9E",
@@ -21,59 +17,66 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 
-basedir = os.path.dirname(__file__)
-
-#Carga la interfaz gráfica y conecta los botones
-class Ventana(QMainWindow):
-    #Inicializamos la ventana y conectamos los botones
-    def __init__(self):
-
-        super(Ventana, self).__init__() 
+class Login(QMainWindow):
+    def __init__(self, stacked_widget):
+        super(Login, self).__init__()
+        uic.loadUi(os.path.join(basedir, 'login.ui'), self)
+        self.setWindowTitle("Anigiri")
         
-        self.isResult = False
-
-        uic.loadUi(os.path.join(basedir, 'login.ui'),self)
-        self.setWindowTitle("Anigiri") 
-
-
+        self.stacked_widget = stacked_widget  # Referencia al `QStackedWidget`
+        
         self.loginButton.clicked.connect(self.login)
         self.registerButton.clicked.connect(self.register)
 
-        self.history = dict()
-
-        # Conectamos los botones
-    
     def login(self):
         email = self.emailText.toPlainText()
         password = self.passwordText.toPlainText()
 
-        auth = firebase.auth()
         try:
             user = auth.sign_in_with_email_and_password(email, password)
-            self.user = user
-            print(user)
-            
+            print("Login exitoso:", user)
+
+            # Cambia a la pantalla de inicio
+            self.stacked_widget.setCurrentIndex(1)
         except:
             QErrorMessage(self).showMessage("Usuario o contraseña incorrectos")
-        
+
     def register(self):
         email = self.emailText.toPlainText()
         password = self.passwordText.toPlainText()
 
-        auth = firebase.auth()
         try:
             user = auth.create_user_with_email_and_password(email, password)
-            self.user = user
-            print(user)
-            
+            print("Usuario registrado:", user)
         except:
             QErrorMessage(self).showMessage("Usuario ya registrado")
 
 
+class HomeScreen(QMainWindow):
+    def __init__(self, stacked_widget):
+        super(HomeScreen, self).__init__()
+        uic.loadUi(os.path.join(basedir, 'homePage.ui'), self)
+        self.setWindowTitle("Anigiri")
+        self.stacked_widget = stacked_widget
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = Ventana()
-    window.show()
-    app.exec() 
+
+    # Creamos un QStackedWidget para manejar las pantallas
+    stacked_widget = QStackedWidget()
+
+    # Creamos las pantallas y las agregamos al QStackedWidget
+    login_screen = Login(stacked_widget)
+    home_screen = HomeScreen(stacked_widget)
+
+    stacked_widget.addWidget(login_screen)  # Índice 0
+    stacked_widget.addWidget(home_screen)   # Índice 1
+
+    stacked_widget.setCurrentIndex(0)  # Empezamos en la pantalla de Login
+    stacked_widget.show()
+
+    app.exec()
