817b975905a8fb38fbeaa45872370a7b49c4f50a
lnarote777 <lnarote777@iesrafaelalberti.es>
2025-03-03 16:25:23 +0100

    HomePage

--- homePage_ui.py
+++ homePage_ui.py
@@ -15,104 +15,189 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QMainWindow,
-    QPushButton, QSizePolicy, QSpacerItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
+    QMainWindow, QPushButton, QSizePolicy, QSpacerItem,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 589)
+        MainWindow.resize(785, 693)
         MainWindow.setStyleSheet(u"background-color: rgb(21, 32, 60);")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.verticalSpacer_6 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.verticalSpacer_5 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.gridLayout.addItem(self.verticalSpacer_6, 11, 1, 1, 1)
-
-        self.verticalSpacer_8 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer_8, 9, 1, 1, 1)
-
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-
-        self.gridLayout.addWidget(self.pushButton, 0, 3, 1, 1)
-
-        self.pushButton_2 = QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-
-        self.gridLayout.addWidget(self.pushButton_2, 0, 4, 1, 1)
-
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer, 2, 1, 1, 1)
-
-        self.pushButton_6 = QPushButton(self.centralwidget)
-        self.pushButton_6.setObjectName(u"pushButton_6")
-
-        self.gridLayout.addWidget(self.pushButton_6, 3, 1, 1, 1)
-
-        self.verticalSpacer_4 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer_4, 13, 1, 1, 1)
-
-        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer_2, 15, 1, 1, 1)
-
-        self.pushButton_3 = QPushButton(self.centralwidget)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-
-        self.gridLayout.addWidget(self.pushButton_3, 7, 1, 1, 1)
+        self.gridLayout.addItem(self.verticalSpacer_5, 17, 1, 1, 1)
 
         self.pushButton_4 = QPushButton(self.centralwidget)
         self.pushButton_4.setObjectName(u"pushButton_4")
 
-        self.gridLayout.addWidget(self.pushButton_4, 6, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_4, 7, 1, 1, 1)
+
+        self.verticalSpacer_4 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer_4, 18, 1, 1, 1)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer_2, 20, 1, 1, 1)
 
         self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.gridLayout.addItem(self.verticalSpacer_3, 14, 1, 1, 1)
+        self.gridLayout.addItem(self.verticalSpacer_3, 19, 1, 1, 1)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer_2, 1, 3, 1, 1)
+
+        self.mangaAPI = QPushButton(self.centralwidget)
+        self.mangaAPI.setObjectName(u"mangaAPI")
+
+        self.gridLayout.addWidget(self.mangaAPI, 6, 1, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.gridLayout.addItem(self.horizontalSpacer, 0, 3, 1, 1)
+
+        self.verticalSpacer_6 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer_6, 16, 1, 1, 1)
 
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
         self.label.setStyleSheet(u"font: 24pt \"Pinky Show\";")
 
-        self.gridLayout.addWidget(self.label, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.label, 0, 1, 2, 1)
 
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.animeAPI = QPushButton(self.centralwidget)
+        self.animeAPI.setObjectName(u"animeAPI")
 
-        self.gridLayout.addItem(self.horizontalSpacer, 0, 2, 1, 1)
+        self.gridLayout.addWidget(self.animeAPI, 5, 1, 1, 1)
 
-        self.verticalSpacer_9 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer_9, 8, 1, 1, 1)
-
-        self.pushButton_5 = QPushButton(self.centralwidget)
-        self.pushButton_5.setObjectName(u"pushButton_5")
-
-        self.gridLayout.addWidget(self.pushButton_5, 5, 1, 1, 1)
-
-        self.verticalSpacer_5 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer_5, 12, 1, 1, 1)
-
-        self.verticalSpacer_7 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
-
-        self.gridLayout.addItem(self.verticalSpacer_7, 10, 1, 1, 1)
-
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.gridLayout.addItem(self.horizontalSpacer_2, 1, 2, 1, 1)
-
+        self.gridLayout_3 = QGridLayout()
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
         self.widget = QWidget(self.centralwidget)
         self.widget.setObjectName(u"widget")
-        self.widget.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.widget.setStyleSheet(u"background-color: rgb(127, 127, 127);")
+        self.gridLayout_2 = QGridLayout(self.widget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.label_3 = QLabel(self.widget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setStyleSheet(u"")
 
-        self.gridLayout.addWidget(self.widget, 2, 2, 14, 3)
+        self.gridLayout_2.addWidget(self.label_3, 4, 0, 1, 1)
+
+        self.label_2 = QLabel(self.widget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setStyleSheet(u"")
+
+        self.gridLayout_2.addWidget(self.label_2, 0, 0, 1, 1)
+
+        self.line_4 = QFrame(self.widget)
+        self.line_4.setObjectName(u"line_4")
+        self.line_4.setFrameShape(QFrame.Shape.HLine)
+        self.line_4.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_4, 1, 0, 1, 1)
+
+        self.line = QFrame(self.widget)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShape(QFrame.Shape.HLine)
+        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout_2.addWidget(self.line, 5, 0, 1, 1)
+
+        self.widget_2 = QWidget(self.widget)
+        self.widget_2.setObjectName(u"widget_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget_2.sizePolicy().hasHeightForWidth())
+        self.widget_2.setSizePolicy(sizePolicy)
+        self.widget_2.setStyleSheet(u"background-color: rgb(0, 0, 0);")
+        self.gridLayout_5 = QGridLayout(self.widget_2)
+        self.gridLayout_5.setObjectName(u"gridLayout_5")
+        self.pushButton_31 = QPushButton(self.widget_2)
+        self.pushButton_31.setObjectName(u"pushButton_31")
+        self.pushButton_31.setStyleSheet(u"background-color: rgb(108, 199, 255);")
+
+        self.gridLayout_5.addWidget(self.pushButton_31, 0, 0, 1, 1)
+
+        self.pushButton_42 = QPushButton(self.widget_2)
+        self.pushButton_42.setObjectName(u"pushButton_42")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.pushButton_42.sizePolicy().hasHeightForWidth())
+        self.pushButton_42.setSizePolicy(sizePolicy1)
+        self.pushButton_42.setStyleSheet(u"background-color: rgb(108, 199, 255);")
+
+        self.gridLayout_5.addWidget(self.pushButton_42, 0, 1, 1, 1)
+
+        self.pushButton_33 = QPushButton(self.widget_2)
+        self.pushButton_33.setObjectName(u"pushButton_33")
+        self.pushButton_33.setStyleSheet(u"background-color: rgb(108, 199, 255);")
+
+        self.gridLayout_5.addWidget(self.pushButton_33, 0, 2, 1, 1)
+
+
+        self.gridLayout_2.addWidget(self.widget_2, 6, 0, 1, 1)
+
+        self.line_3 = QFrame(self.widget)
+        self.line_3.setObjectName(u"line_3")
+        self.line_3.setFrameShape(QFrame.Shape.HLine)
+        self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout_2.addWidget(self.line_3, 7, 0, 1, 1)
+
+        self.widget1 = QWidget(self.widget)
+        self.widget1.setObjectName(u"widget1")
+        sizePolicy.setHeightForWidth(self.widget1.sizePolicy().hasHeightForWidth())
+        self.widget1.setSizePolicy(sizePolicy)
+        self.widget1.setStyleSheet(u"background-color: rgb(0, 0, 0);")
+        self.gridLayout_4 = QGridLayout(self.widget1)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.pushButton_37 = QPushButton(self.widget1)
+        self.pushButton_37.setObjectName(u"pushButton_37")
+        self.pushButton_37.setStyleSheet(u"background-color: rgb(108, 199, 255);")
+
+        self.gridLayout_4.addWidget(self.pushButton_37, 0, 0, 1, 1)
+
+        self.pushButton_38 = QPushButton(self.widget1)
+        self.pushButton_38.setObjectName(u"pushButton_38")
+        sizePolicy1.setHeightForWidth(self.pushButton_38.sizePolicy().hasHeightForWidth())
+        self.pushButton_38.setSizePolicy(sizePolicy1)
+        self.pushButton_38.setStyleSheet(u"background-color: rgb(108, 199, 255);")
+
+        self.gridLayout_4.addWidget(self.pushButton_38, 0, 1, 1, 1)
+
+        self.pushButton_39 = QPushButton(self.widget1)
+        self.pushButton_39.setObjectName(u"pushButton_39")
+        self.pushButton_39.setStyleSheet(u"background-color: rgb(108, 199, 255);")
+
+        self.gridLayout_4.addWidget(self.pushButton_39, 0, 2, 1, 1)
+
+
+        self.gridLayout_2.addWidget(self.widget1, 2, 0, 1, 1)
+
+
+        self.gridLayout_3.addWidget(self.widget, 0, 0, 1, 1)
+
+
+        self.gridLayout.addLayout(self.gridLayout_3, 3, 2, 18, 2)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.gridLayout.addItem(self.verticalSpacer, 3, 1, 1, 1)
+
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.gridLayout.addWidget(self.pushButton_3, 8, 1, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -123,12 +208,18 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"login", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"sign up", None))
-        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Estudios", None))
+        self.mangaAPI.setText(QCoreApplication.translate("MainWindow", u"Mangas", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p>Anigiri</p></body></html>", None))
-        self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.animeAPI.setText(QCoreApplication.translate("MainWindow", u"Animes", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:18pt;\">Mangas</span></p></body></html>", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:18pt;\">Animes</span></p></body></html>", None))
+        self.pushButton_31.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_42.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_33.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_37.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_38.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_39.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Mangakas", None))
     # retranslateUi
 
--- login_ui.py
+++ login_ui.py
@@ -33,17 +33,17 @@ class Ui_MainWindow(object):
 
         self.gridLayout_5.addItem(self.horizontalSpacer_6, 5, 2, 1, 1)
 
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
+        self.Title = QLabel(self.centralwidget)
+        self.Title.setObjectName(u"Title")
         font = QFont()
         font.setFamilies([u"Pinky Show"])
         font.setPointSize(24)
         font.setBold(False)
         font.setItalic(False)
-        self.label_2.setFont(font)
-        self.label_2.setStyleSheet(u"font: 24pt \"Pinky Show\";")
+        self.Title.setFont(font)
+        self.Title.setStyleSheet(u"font: 24pt \"Pinky Show\";")
 
-        self.gridLayout_5.addWidget(self.label_2, 1, 2, 1, 1)
+        self.gridLayout_5.addWidget(self.Title, 1, 2, 1, 1)
 
         self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
@@ -51,14 +51,14 @@ class Ui_MainWindow(object):
 
         self.gridLayout_4 = QGridLayout()
         self.gridLayout_4.setObjectName(u"gridLayout_4")
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setMinimumSize(QSize(200, 50))
-        self.pushButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+        self.registerButton = QPushButton(self.centralwidget)
+        self.registerButton.setObjectName(u"registerButton")
+        self.registerButton.setMinimumSize(QSize(200, 50))
+        self.registerButton.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
 "font: 700 11pt \"Segoe UI\";\n"
 "color: rgb(36, 61, 95);")
 
-        self.gridLayout_4.addWidget(self.pushButton, 0, 4, 1, 1)
+        self.gridLayout_4.addWidget(self.registerButton, 0, 4, 1, 1)
 
         self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
@@ -86,48 +86,52 @@ class Ui_MainWindow(object):
 
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label_3 = QLabel(self.centralwidget)
-        self.label_3.setObjectName(u"label_3")
+        self.email = QLabel(self.centralwidget)
+        self.email.setObjectName(u"email")
 
-        self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.email, 1, 0, 1, 1)
 
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
+        self.Pregunta = QLabel(self.centralwidget)
+        self.Pregunta.setObjectName(u"Pregunta")
 
-        self.gridLayout.addWidget(self.label, 10, 1, 1, 1)
+        self.gridLayout.addWidget(self.Pregunta, 10, 1, 1, 1)
 
         self.VisiblePass = QCheckBox(self.centralwidget)
         self.VisiblePass.setObjectName(u"VisiblePass")
 
         self.gridLayout.addWidget(self.VisiblePass, 10, 0, 1, 1)
 
-        self.password = QTextEdit(self.centralwidget)
-        self.password.setObjectName(u"password")
-        self.password.setMaximumSize(QSize(16777215, 36))
-        self.password.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+        self.passwordText = QTextEdit(self.centralwidget)
+        self.passwordText.setObjectName(u"passwordText")
+        self.passwordText.setMaximumSize(QSize(16777215, 36))
+        self.passwordText.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
 "font: 16pt \"Segoe UI\";\n"
 "color: rgb(0, 0, 0);")
-        self.password.setFrameShape(QFrame.Shape.StyledPanel)
+        self.passwordText.setFrameShape(QFrame.Shape.StyledPanel)
+        self.passwordText.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.passwordText.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
-        self.gridLayout.addWidget(self.password, 9, 0, 1, 2)
+        self.gridLayout.addWidget(self.passwordText, 9, 0, 1, 2)
 
-        self.label_4 = QLabel(self.centralwidget)
-        self.label_4.setObjectName(u"label_4")
+        self.password_2 = QLabel(self.centralwidget)
+        self.password_2.setObjectName(u"password_2")
 
-        self.gridLayout.addWidget(self.label_4, 7, 0, 1, 2)
+        self.gridLayout.addWidget(self.password_2, 7, 0, 1, 2)
 
-        self.username = QTextEdit(self.centralwidget)
-        self.username.setObjectName(u"username")
-        self.username.setMaximumSize(QSize(16777215, 36))
-        self.username.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
-        self.username.setAutoFillBackground(True)
-        self.username.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+        self.emailText = QTextEdit(self.centralwidget)
+        self.emailText.setObjectName(u"emailText")
+        self.emailText.setMaximumSize(QSize(16777215, 36))
+        self.emailText.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.emailText.setAutoFillBackground(True)
+        self.emailText.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
 "font: 16pt \"Segoe UI\";\n"
 "color: rgb(0, 0, 0);")
-        self.username.setFrameShape(QFrame.Shape.StyledPanel)
-        self.username.setFrameShadow(QFrame.Shadow.Sunken)
+        self.emailText.setFrameShape(QFrame.Shape.StyledPanel)
+        self.emailText.setFrameShadow(QFrame.Shadow.Sunken)
+        self.emailText.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.emailText.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
-        self.gridLayout.addWidget(self.username, 2, 0, 1, 2)
+        self.gridLayout.addWidget(self.emailText, 2, 0, 1, 2)
 
         self.verticalSpacer_5 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
@@ -176,13 +180,13 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\">Anigiri</p></body></html>", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Registrarse", None))
+        self.Title.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\">Anigiri</p></body></html>", None))
+        self.registerButton.setText(QCoreApplication.translate("MainWindow", u"Registrarse", None))
         self.loginButton.setText(QCoreApplication.translate("MainWindow", u"Iniciar sesi\u00f3n", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Username", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"right\">\u00bfA\u00fan no tienes cuenta? Reg\u00edstrate</p></body></html>", None))
+        self.email.setText(QCoreApplication.translate("MainWindow", u"Email", None))
+        self.Pregunta.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"right\">\u00bfA\u00fan no tienes cuenta? Reg\u00edstrate</p></body></html>", None))
         self.VisiblePass.setText(QCoreApplication.translate("MainWindow", u"Mostrar Contrase\u00f1a", None))
-        self.password.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.passwordText.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "hr { height: 1px; border-width: 0; }\n"
@@ -190,8 +194,8 @@ class Ui_MainWindow(object):
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Segoe UI'; font-size:16pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10.5pt;\"><br /></p></body></html>", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Password", None))
-        self.username.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.password_2.setText(QCoreApplication.translate("MainWindow", u"Password", None))
+        self.emailText.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "hr { height: 1px; border-width: 0; }\n"
