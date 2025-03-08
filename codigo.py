f5d1054753f01473906b54c8aa0386a404b76fa9
rmungar <mgraul@iesrafaelalberti.es>
2025-03-08 20:38:39 +0100

    Con icono

--- codigo.py
+++ codigo.py
@@ -2,15 +2,16 @@
 
 # prueba@gmail.com
 # passwordPrueba
-
+import os
 import sys
 from PyQt6.QtWidgets import QApplication, QStackedWidget
+from PyQt6.QtGui import QIcon
 
 if __name__ == '__main__':
 
     from Screens.LoginScreen import LoginScreen
     app = QApplication(sys.argv)
-
+    app.setWindowIcon(QIcon(f"{os.getcwd()}/Resources/LogoAnigiri.ico"))
     # Crear el QStackedWidget
     stacked_widget = QStackedWidget()
     loginScreen = LoginScreen(stacked_widget)
